4a4483cbe168d7f4136966b783beac0570cd0f67
冒云香 <1084221885@qq.com>
2017-07-05 18:24:55 +0800

    merge

--- slide/index.js
+++ slide/index.js
@@ -4,62 +4,122 @@
 
 (function () {
 
-    /*
-     定时器
-     */
-    var lis = document.querySelectorAll("#imgs li");
-    var points = document.querySelectorAll("#point span");
-    var index = 0;
-    var timer = null;//定时器
-
-
-    function Slide(delay) {
-        //this.lidom = lidom;//lis
-        //this.pointdom = pointdom;
-        this.delay = delay;//延迟时间
+    function Slide(obj) {
+        this.el = obj.el;
+        this.delay = obj.delay;//延迟时间
+        this.img = obj.img;//图片加载
+        this.timer = null;
     }
 
+    Slide.prototype.init = function () {
+        var that = this
+        this.createDom(function () {
+            that.settimer();
+            that.showimg();
+        })
 
-    /*    Slide.prototype.lisdom = function (lis) {
-     return document.querySelectorAll(lis);
-     }
+    }
+    //dom添加
+    Slide.prototype.createDom = function (cb) {
+        var ul = document.createElement("ul");
+        ul.setAttribute("class", "imgs");
+        var div = document.createElement("div");
+        div.setAttribute("class", "point");
+        var imglists = "";
+        var pointlists = "";
+        this.img.forEach(function (item, index, array) {
+            imglists += " <li><img src=" + item.src + " alt=" + item.alt + "></li>";
+            pointlists += "<span>" + (index + 1) + "</span>"
+        })
+        ul.innerHTML = imglists;
+        div.innerHTML = pointlists;
+        var slidediv = document.querySelector(this.el);
+        slidediv.appendChild(ul);
+        slidediv.appendChild(div);
+        console.log(slidediv);
 
-     Slide.prototype.pointsdom = function (point) {
-     return document.querySelectorAll(point);
-     }*/
+        this.$lis = slidediv.querySelectorAll(".imgs li");
+        this.$points = slidediv.querySelectorAll(".point span");
+        console.log(123, this.$lis, this.$points);
+
+
+        this.$index = 0;
+
+        this.$lis[0].style.display = "block";
+        this.$points[0].className = "highlight";
+
+        cb();
+    }
 
     //图片切换函数
     Slide.prototype.changeimg = function (index) {
-        for (var i = 0; i < lis.length; i++) {
-            lis[i].style.display = "none";
-            points[i].className = "";
+        console.log(this.$lis, 222)
+        for (var i = 0; i < this.$lis.length; i++) {
+            this.$lis[i].style.display = "none";
+            this.$points[i].className = "";
         }
-        lis[index].style.display = "block";
-        points[index].className = "highlight";
+        this.$lis[index].style.display = "block";
+        this.$points[index].className = "highlight";
     }
 
     //自动播放
     Slide.prototype.autoimg = function () {
-        if (++index >= lis.length)
-            index = 0;
-        Slide.prototype.changeimg(index);
-        console.log(index);
+        if (++(this.$index) >= this.$lis.length)
+            this.$index = 0;
+        this.changeimg(this.$index);
     }
 
     //设置定时器
-    Slide.prototype.settimer = function (time) {
-        timer = setInterval(function () {
-            a.autoimg()
-        }, time);
+    Slide.prototype.settimer = function () {
+        var that = this;
+        that.timer = setInterval(function () {
+            that.autoimg();
+        }, this.delay);
     }
 
+    //鼠标滑过对应的图片显示
+    Slide.prototype.showimg = function () {
+        var that = this;
+        console.log(222, this.$points);
+        this.$points.forEach(function (item) {
+            item.addEventListener("mouseover", function () {
+                console.log('aaaaaa', that.timer)
+                clearInterval(that.timer);//清除定时器
+                // console.log(this);
+                that.$index = this.innerText - 1;//在mouseover时记录当前的index
+                that.changeimg(that.$index);
+            })
+            item.addEventListener("mouseout", function () {
+                clearInterval(that.timer);
+                //设置定时器
+                that.settimer();
+            })
+        })
+    }
 
-    // var a = new Slide("#imgs li", "#point span", 2000);
-    /* var lis = a.lisdom(a.lidom);
-     var points = a.pointsdom(a.pointdom);*/
+    var a = new Slide({
+        el: "#slide",
+        delay: 2000,
+        img: [
+            {src: "img/test_img01.jpg", alt: "img1"},
+            {src: "img/test_img02.jpg", alt: "img2"},
+            {src: "img/banner.jpg", alt: "img3"},
+            {src: "img/test_img02.jpg", alt: "img2"}
+        ]
+    });
 
-    var a = new Slide(2000);
-    a.settimer(a.delay);
+    a.init();
+
+    var b = new Slide({
+        el: "#mm",
+        delay: 1000,
+        img: [
+            {src: "img/img4.jpg", alt: "img1"},
+            {src: "img/test_img02.jpg", alt: "img2"},
+            {src: "img/img5.jpg", alt: "img2"},
+        ]
+    });
+    b.init();
 
 
     /*
@@ -77,21 +137,6 @@
      })
      }
      */
-
-
-    //foreach遍历points，鼠标滑过，对应的图片显示
-    points.forEach(function (item) {
-        item.addEventListener("mouseover", function () {
-            clearInterval(timer);//清除定时器
-            index = this.innerHTML - 1;//在mouseover时记录当前的index
-            a.changeimg(index);
-            console.log(index);
-        })
-        item.addEventListener("mouseout", function () {
-            clearInterval(timer);
-            a.settimer(a.delay);
-        })
-    })
 
 
 })();
